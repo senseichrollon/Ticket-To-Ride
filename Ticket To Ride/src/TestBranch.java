c1acb8b827b11ce1417620bab6cca4f0796ab08e
slhscs357 <slhscs357@T238377.slhs.katyisd.org>
2019-02-22 13:55:45 -0600

    Test

--- Ticket To Ride/src/TestBranch.java
+++ Ticket To Ride/src/TestBranch.java
@@ -1,9 +1,9 @@
 
-public class TestBranch {
-
-	public static void main(String[] args) {
+public class TestBranch 
+{
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-
+		System.out.println("Nikunj");
 	}
-
 }
