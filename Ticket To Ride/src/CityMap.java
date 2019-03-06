d4fe1cb37be8bce059188f3a76ae2357f42e8873
RackS103 <racsharma09@gmail.com>
2019-03-06 15:15:31 -0600

    RM, 3/6, Added tickets.txt

--- Ticket To Ride/src/CityMap.java
+++ Ticket To Ride/src/CityMap.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class CityMap 
 {
-	
+	private List<ArrayList<Track>> map;
+	public static HashMap<String, Integer> INDEX;
 	
 	public CityMap()
-	{}
+	{
+		
+	}
 	 
 	
 	
