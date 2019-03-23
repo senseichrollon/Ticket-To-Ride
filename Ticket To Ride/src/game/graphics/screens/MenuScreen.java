8f37ae8ec97e59c8a882860769cbe61bcb620827
senseichrollon <42702456+senseichrollon@users.noreply.github.com>
2019-03-23 10:50:07 -0500

    AR 10:49 3/24 : MenuScreen

--- Ticket To Ride/src/game/graphics/screens/MenuScreen.java
+++ Ticket To Ride/src/game/graphics/screens/MenuScreen.java
@@ -3,10 +3,16 @@ package game.graphics.screens;
 import java.awt.Graphics2D;
 
 import game.graphics.input.MouseInput;
+import game.graphics.util.MButton;
 
 public class MenuScreen extends ScreenManager {
 
 	private MouseInput input;
+	private MButton playButton;
+	
+	public MenuScreen() {
+		
+	}
 	
 	@Override
 	public void update() {
