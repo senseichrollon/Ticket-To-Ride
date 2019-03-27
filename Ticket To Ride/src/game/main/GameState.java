cf113474224ca022c008537c7c32d13ff2225bc1
slhscs357 <slhscs357@T238377.slhs.katyisd.org>
2019-03-27 14:31:59 -0500

    Gamestate

--- Ticket To Ride/src/game/main/GameState.java
+++ Ticket To Ride/src/game/main/GameState.java
@@ -1,7 +1,47 @@
 package game.main;
-import java.util.*;
-public class GameState {
-	public static void main(String[] args) {
-		
+import game.entity.CityMap;
+import game.entity.Deck;
+import game.entity.Player;
+
+public class GameState 
+{
+	private Player[] players;
+	private int currentPlayer, numCardsDrawn;
+	private CityMap board;
+	private Deck deck;
+	public static int NUMPLAYERS = 4;
+	
+	public GameState()
+	{
+		players = new Player[4];
+		players[0] = new Player("Jim", "blue");
+		players[1] = new Player("Joe", "purple");
+		players[2] = new Player("Bob", "green");
+		players[3] = new Player("John", "yellow");
+		currentPlayer = (int)(Math.random() * 5);
+		numCardsDrawn = 0;
+		board = new CityMap();
+		deck = new Deck();
 	}
+
+	public Player[] getPlayers() 
+	{
+		return players;
+	}
+
+	public int getCurrentPlayer()
+	{
+		return currentPlayer;
+	}
+
+	public CityMap getBoard() 
+	{
+		return board;
+	}
+
+	public Deck getDeck() 
+	{
+		return deck;
+	}
+
 }
