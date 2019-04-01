9b10fd973a931036dc83a07a45edac39663ed4af
RackS103 <racsharma09@gmail.com>
2019-04-01 14:36:23 -0500

    Boost Track, 4/1, RM

--- Ticket To Ride/src/game/entity/Track.java
+++ Ticket To Ride/src/game/entity/Track.java
@@ -3,20 +3,24 @@ package game.entity;
 public class Track {
 	private String cityOne;
 	private String cityTwo;
-	private String color1;
-	private String color2;
+	private String trackColor1;
+	private String trackColor2;
+	private String playerColor1;
+	private String playerColor2;
 	private int id;
 
 	public Track(int id, String cityOne, String cityTwo, String color) {
 		this(id, cityOne, cityTwo, color, null);
 	}
 	
-	public Track(int id, String cityOne, String cityTwo, String color1, String color2) {
+	public Track(int id, String cityOne, String cityTwo, String trackColor1, String trackColor2) {
 		this.id = id;
 		this.cityOne = cityOne;
 		this.cityTwo = cityTwo;
-		this.color1 = color1;
-		this.color2 = color2;
+		this.trackColor1 = trackColor1;
+		this.trackColor2 = trackColor2;
+		playerColor1 = null;
+		playerColor2 = null;
 	}
 
 	public String getCityOne() {
@@ -43,20 +47,77 @@ public class Track {
 		return "";
 	}
 
-	public String getColorOne() {
-		return color1;
+	public String getTrackColor1() {
+		return trackColor1;
 	}
 
-	public void setColorOne(String color) {
-		this.color1 = color;
+	public void setTrackColor1(String trackColor1) {
+		this.trackColor1 = trackColor1;
+	}
+
+	public String getTrackColor2() {
+		return trackColor2;
+	}
+
+	public void setTrackColor2(String trackColor2) {
+		this.trackColor2 = trackColor2;
+	}
+
+	public String getPlayerColor1() {
+		return playerColor1;
+	}
+
+	public void setPlayerColor1(String playerColor1) {
+		this.playerColor1 = playerColor1;
+	}
+
+	public String getPlayerColor2() {
+		return playerColor2;
 	}
 	
-	public String getColorTwo() {
-		return color2;
+	public boolean contains(String col)
+	{
+		if(trackColor1.equals(col) || (trackColor2 != null && trackColor2.equals(col)))
+			return true;
+		return false;
 	}
-
-	public void setColorTwo(String color) {
-		this.color2 = color;
+	
+	public boolean isFilled()
+	{
+		if(trackColor2 == null)
+		{
+			if(playerColor1 != null)
+				return true;
+		}
+		else
+		{
+			if(playerColor1 != null && playerColor2 != null)
+				return true;
+		}
+		return false;
 	}
-
+	
+	public boolean occupyTrack(String pc, String colChoice)
+	{
+		if(trackColor2 == null)
+		{
+			if(playerColor1 == null)
+			{
+				playerColor1 = pc;
+				return true;
+			}
+		}
+		else
+		{
+			if(contains(colChoice))
+			{
+				if(trackColor1.equals(colChoice) && playerColor1 == null 
+				&& (playerColor2 == null || !playerColor2.equals(pc))) 
+				{
+					
+				}
+			}
+		}
+		return false;
+	}
 }
