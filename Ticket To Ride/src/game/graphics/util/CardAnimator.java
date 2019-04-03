e4b075f742e0164a7c48cb732bc3c98bbd85c25a
slhscs350 <slhscs350@T238380.slhs.katyisd.org>
2019-04-03 16:04:27 -0500

    lol

--- Ticket To Ride/src/game/graphics/util/CardAnimator.java
+++ Ticket To Ride/src/game/graphics/util/CardAnimator.java
@@ -1,6 +1,7 @@
 package game.graphics.util;
 
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class CardAnimator {
 	private int startX;
@@ -8,15 +9,41 @@ public class CardAnimator {
 	
 	private int endX;
 	private int endY;
+
+	private long startTime;
+	private long duration;
 	
-	private int cX;
-	private int cY;
+	private BufferedImage cardImage;
+	private boolean running = false;
 	
-	public void update() {
-		
+	public CardAnimator(int sx, int sy, int ex, int ey, BufferedImage img, long dur) {
+		startX = sx;
+		startY = sy;
+		endX = ex;
+		endY = ey;
+		cardImage = img;
+		duration = dur;
 	}
+	
+	public void start() {
+		running = true;
+		startTime = System.nanoTime();
+	}
+	
 	
 	public void draw(Graphics2D g) {
 		
+	}
+	
+	public boolean hasStarted() {
+		return running;
+	}
+	
+	public boolean canEnd() {
+		return System.nanoTime() - startTime >= duration;
+	}
+	
+	public void stop() {
+		running = false;
 	}
 }
