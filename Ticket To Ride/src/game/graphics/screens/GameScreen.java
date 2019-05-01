812b0d55adb4e1e5ae27b761a0a954106bd4d5c2
RackS103 <racsharma09@gmail.com>
2019-05-01 13:53:52 -0500

    adka;sdjaavel/aevbkiwjefwioaed  "

--- Ticket To Ride/src/game/graphics/screens/GameScreen.java
+++ Ticket To Ride/src/game/graphics/screens/GameScreen.java
@@ -8,20 +8,24 @@ import java.awt.Paint;
 import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.stream.IntStream;
 
 import game.entity.ContractCard;
 import game.entity.Deck;
 import game.entity.Player;
+import game.entity.Track;
+import game.graphics.animation.AnimationManager;
 import game.graphics.drawers.CityMapDrawer;
 import game.graphics.drawers.ContractCardDrawer;
 import game.graphics.drawers.HandDrawer;
+import game.graphics.drawers.TrainBackGround;
 import game.graphics.engine.GraphicsPanel;
 import game.graphics.input.InputManager;
 import game.graphics.input.MouseInput;
@@ -37,23 +41,33 @@ public class GameScreen extends ScreenManager implements Runnable {
 
 	private Thread gameThread;
 	private BufferedImage logo, govContract, trainContract;
-	private LinkedHashMap<String, BufferedImage> cards;
+	private  LinkedHashMap<String, BufferedImage> cards;
 
 	private InputManager input;
+	private boolean init;
 
 	public GameScreen(MouseInput in) {
+		input = new InputManager(in);
+		logo = ImageLoader.loadImage("resources/menuscreen/logo2.png");
+		logo = ImageLoader.resize(logo, logo.getWidth() / 3, logo.getHeight() / 3);
+		govContract = ImageLoader.loadImage("resources/contractcard/ticket_card_back.jpg");
+		trainContract = ImageLoader.loadImage("resources/traincards/backtrain.png");
+		AnimationManager.init();
+	}
+	
+	public void startGame() {
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	public void initGame() {
+		init = true;
 		try {
 			game = new GameState();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		input = new InputManager(in);
 		cMapDrawer = new CityMapDrawer(game.getBoard());
-		logo = ImageLoader.loadImage("resources/menuscreen/logo2.png");
-		logo = ImageLoader.resize(logo, logo.getWidth() / 3, logo.getHeight() / 3);
-		govContract = ImageLoader.loadImage("resources/contractcard/ticket_card_back.jpg");
-		trainContract = ImageLoader.loadImage("resources/traincards/backtrain.png");
 		contractDrawer = new ContractCardDrawer(game.getDeck().getContractCards());
 
 		cards = new LinkedHashMap<>();
@@ -64,31 +78,37 @@ public class GameScreen extends ScreenManager implements Runnable {
 				cards.put(color, ImageLoader.loadImage("resources/traincards/" + color + ".png"));
 			else
 				cards.put(color, ImageLoader.loadImage("resources/traincards/" + color + ".jpg"));
+
 		}
 
 		handDrawer = new HandDrawer(cards);
 		handDrawer.setTree(game.getPlayers()[game.getCurrentPlayer()].getCards());
-
-		gameThread = new Thread(this);
-		gameThread.start();
+		init = false;
 	}
-
 	@Override
 	public void run() {
-		while (!game.hasWinner()) {
-			int num = input.requestTypeOfTurn(game.getPlayers()[game.getCurrentPlayer()].getName());
+		initGame();
+		for(int i = 0; i < game.getPlayers().length; i++) {
+			requestGovContract();
+			input.reset();
+			try {Thread.sleep(1000);} catch (InterruptedException e) {	}
+			game.updatePlayer();
+
+		}
+		while (game.hasWinner() <= 4) {
+			int num = input.requestTypeOfTurn(game);
 			input.reset();
 			switch (num) {
 				case 1: {
-					Rectangle[] coords = new Rectangle[6];
+					Rectangle2D.Double[] coords = new Rectangle2D.Double[6];
 					int y = 20;
 					String[] upTrains = game.getDeck().getUpCards();
 					for(int i = 0; i < 5; i++) {
 						BufferedImage img = cards.get(upTrains[i]);
-						coords[i] = new Rectangle(1700,y,(img.getWidth()*8)/10,(img.getHeight()*8)/10);
+						coords[i] = new Rectangle2D.Double(1700,y,(img.getWidth()*8)/10,(img.getHeight()*8)/10);
 						y+= 130;
 					}
-					coords[5] = new Rectangle(1900- trainContract.getHeight()/4,760,(trainContract.getHeight())/4,(trainContract.getWidth())/4);
+					coords[5] = new Rectangle2D.Double(1900- trainContract.getHeight()/4,760,(trainContract.getHeight())/4,(trainContract.getWidth())/4);
 					while(game.getNumCardsDrawn() < 2) {
 						int index = input.requestTrainCardSelection(coords,game.getNumCardsDrawn(),upTrains);
 						if(index < 5)
@@ -101,26 +121,27 @@ public class GameScreen extends ScreenManager implements Runnable {
 					break;
 				}
 				case 2: {
-
+					int id = input.requestTrack(cMapDrawer.getDrawMap(), game.getPlacableTracks());
+					input.reset();
+					Track track = game.getBoard().getTrack(id > 100? id-1000:id);
+					HashMap<String,Integer> cards = input.requestCards(track, id>100, handDrawer.getCards(),game.getPlayers()[game.getCurrentPlayer()]);
+					int wildCount = 0;
+					String color = "";
+					int colorCount = 0;
+					
+					for(String s : cards.keySet()) {
+						if(s.equals("wild")) {
+							wildCount = cards.get(s);
+						} else {
+							color = s;
+							colorCount = cards.get(s);
+						}
+					}
+					game.placeTrack(track, color, colorCount, wildCount, id>100);
+					break;
 				}
 				case 3: {
-					ContractCard[] cards = game.drawContracts();
-					BufferedImage[] img = new BufferedImage[cards.length];
-					for(int i = 0; i < cards.length; i++) {
-						img[i] = ImageLoader.getCopy(contractDrawer.getCardImages().get(cards[i]));
-					}
-					ArrayList<Integer> keep = input.requestGovernmentContract(cards, img);
-					ArrayList<ContractCard> retCards = new ArrayList<ContractCard>();
-					ArrayList<ContractCard> keepCards = new ArrayList<ContractCard>();
-					for(int i = 0; i < cards.length; i++) {
-						if(keep.contains(i))
-							keepCards.add(cards[i]);
-						else
-							retCards.add(cards[i]);
-					}
-
-					game.setContracts(keepCards);
-					game.returnContracts(retCards);
+					requestGovContract();
 				}
 			}
 			input.reset();
@@ -132,17 +153,44 @@ public class GameScreen extends ScreenManager implements Runnable {
 			}
 			game.updatePlayer();
 		}
+		
+		int[][] mat = game.endGame();
+	}
+	
+	public void requestGovContract() {
+		ContractCard[] cards = game.drawContracts();
+		BufferedImage[] img = new BufferedImage[cards.length];
+		for(int i = 0; i < cards.length; i++) {
+			img[i] = ImageLoader.getCopy(contractDrawer.getCardImages().get(cards[i]));
+		}
+		ArrayList<Integer> keep = input.requestGovernmentContract(cards, img);
+		ArrayList<ContractCard> retCards = new ArrayList<ContractCard>();
+		ArrayList<ContractCard> keepCards = new ArrayList<ContractCard>();
+		for(int i = 0; i < cards.length; i++) {
+			if(keep.contains(i))
+				keepCards.add(cards[i]);
+			else
+				retCards.add(cards[i]);
+		}
+
+		game.setContracts(keepCards);
+		game.returnContracts(retCards);
+		
 	}
 
 	@Override
 	public void update() {
-		
+		if(init) {
+			TrainBackGround.update();
+			return;
+		}
 		input.update();
 		if (!(contractDrawer.getParent() == GraphicsPanel.getPanel())) {
 			GraphicsPanel.getPanel().add(contractDrawer);
 		}
 		handDrawer.setTree(game.getPlayers()[game.getCurrentPlayer()].getCards());
 		contractDrawer.setPlayerContracts(game.getPlayers()[game.getCurrentPlayer()].getContracts());
+		AnimationManager.update();
 	}
 
 	public void drawPiles(Graphics2D g) {
@@ -173,12 +221,12 @@ public class GameScreen extends ScreenManager implements Runnable {
 		IntStream.range(0, players.length).forEach(i -> sorted.add(players[i]));
 		Collections.sort(sorted, (a,b) -> Integer.compare(b.getPoints(), a.getPoints()));
 		g.setColor(Color.BLACK);
-		g.setFont(new Font("TimesRoman", Font.BOLD | Font.ITALIC, 25));
-		g.drawString(String.format("%-8s%-8s%-8s","Player","Points","Trains"), 20, 80);
+		g.setFont(new Font("TimesRoman", Font.BOLD | Font.ITALIC, 15));
+		g.drawString(String.format("%8s   %8s %8s   %14s    %14s","","Points","Trains","Train Cards","Contract Cards"), 20, 80);
 		int y = 150;
-		for(Player p : players) {
-			g.setColor(Color.BLACK);
-			g.drawString(String.format("%-8s%4d  %8d",p.getName(), p.getPoints(),p.getTrains()), 20, y);
+		for(Player p : sorted) {
+			g.setColor(p == players[game.getCurrentPlayer()]?Color.WHITE:Color.BLACK);
+			g.drawString(String.format("%-8s     %4d    %8d      %12d   %21d",p.getName(), p.getPoints(),p.getTrains(),p.getCards().getNumCards(),p.getContracts().size()), 20, y);
 			Color c = null;
 			switch(p.getTrainColor()) {
 				case "blue": {
@@ -190,7 +238,7 @@ public class GameScreen extends ScreenManager implements Runnable {
 					break;
 				}
 				case "green": {
-					c = Color.GREEN;
+					c = Color.GREEN.darker();
 					break;
 				}
 				case "yellow": {
@@ -199,13 +247,19 @@ public class GameScreen extends ScreenManager implements Runnable {
 				}
 			}
 			g.setColor(c);
-			g.fill(new Ellipse2D.Double(230,y-20,30,30));
+			g.fill(new Ellipse2D.Double(360,y-20,30,30));
 			y+= 70;
 		}
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
+		if(init) {
+			TrainBackGround.draw(g);
+			g.setFont(new Font("TimesRoman", Font.BOLD, 60));
+			g.drawString("Loading game", 800, 650);
+			return;
+		}
 		handDrawer.draw(g);
 		Color c2 = Color.RED.darker();
 		GradientPaint gp1 = new GradientPaint(0, 0, Color.orange, 0, (600), c2, true);
@@ -223,7 +277,9 @@ public class GameScreen extends ScreenManager implements Runnable {
 		cMapDrawer.draw(g);
 		input.draw(g);
 		drawLeaderBoard(g);
+		AnimationManager.draw(g);
 		
 	}
+	
 
 }
