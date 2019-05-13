a61318dd15750a1b79c7b791b814d2dfe0668703
RackS103 <racsharma09@gmail.com>
2019-05-12 21:22:40 -0500

    Game Ending Message

--- Ticket To Ride/src/game/testing/TestMap.java
+++ Ticket To Ride/src/game/testing/TestMap.java
@@ -12,36 +12,30 @@ public class TestMap
 	public static void main(String[] args) throws IOException 
 	{
 		CityMap cm = new CityMap();
+		cm.addTrack(cm.getTrack("Calgary", "Helena"), "Bob", "gray", 1);
+		cm.addTrack(cm.getTrack("Denver", "Helena"), "Bob", "green", 1);
+		cm.addTrack(cm.getTrack("Denver", "Salt Lake City"), "Bob", "yellow", 1);
+		cm.addTrack(cm.getTrack("Denver", "Kansas City"), "Bob", "black", 1);
+		cm.addTrack(cm.getTrack("Kansas City", "Saint Louis"), "Bob", "purple", 1);
+		cm.addTrack(cm.getTrack("Saint Louis", "Nashville"), "Bob", "gray", 1);
 		
-		//cm.addTrack("Seattle", "Helena" , "Yeet", null);
+		cm.addTrack(cm.getTrack("Duluth", "Omaha"), "Gary", "gray", 1);
+		cm.addTrack(cm.getTrack("Kansas City", "Omaha"), "Gary", "gray", 1);
+		cm.addTrack(cm.getTrack("Kansas City", "Oklahoma City"), "Gary", "gray", 1);
 		
-		cm.addTrack(cm.getTrack("Nashville", "Raleigh"), "Yeet", null, 1);
-		cm.addTrack(cm.getTrack("Pittsburgh", "Raleigh"), "Yeet", null, 1);
-		cm.addTrack(cm.getTrack("Pittsburgh", "New York"), "Yeet", "green", 1);
-		cm.addTrack(cm.getTrack("Washington", "New York"), "Yeet", "orange", 1);
-		cm.addTrack(cm.getTrack("Boston", "New York"), "Yeet", "red", 1);
-		cm.addTrack(cm.getTrack("Salt Lake City", "Denver"), "Yeet", "red", 1);
-		cm.addTrack(cm.getTrack("Denver", "Kansas City"), "Yeet", "black", 1);
+		cm.addTrack(cm.getTrack("Duluth", "Omaha"), "Ron", "gray", 2);
+		cm.addTrack(cm.getTrack("Denver", "Omaha"), "Ron", "purple", 1);
+		cm.addTrack(cm.getTrack("Denver", "Santa Fe"), "Ron", "gray", 1);
+		cm.addTrack(cm.getTrack("Santa Fe", "El Paso"), "Ron", "gray", 1);
 		
-		/*cm.addTrack("Calgary", "Helena" , "Yeet", null);
-		cm.addTrack("Helena", "Winnipeg" , "Yeet", null);
-		cm.addTrack("Winnipeg", "Duluth", "Yeet", null);
-		cm.addTrack("Helena", "Duluth", "Yeet", null);
-		cm.addTrack("Helena", "Omaha" , "Yeet", null);
-		cm.addTrack("Omaha", "Denver" , "Yeet", null);
-		cm.addTrack("Helena", "Denver" , "Yeet", null);*/
+		cm.addTrack(cm.getTrack("Seattle", "Portland"), "Yeet", "gray", 2);
+		cm.addTrack(cm.getTrack("San Francisco", "Portland"), "Yeet", "purple", 2);
+		cm.addTrack(cm.getTrack("San Francisco", "Los Angeles"), "Yeet", "yellow", 1);
 		
-		//cm.addTrack("Toronto", "Duluth" , "Yeet", null);
-		//cm.addTrack("Las_Vegas", "Salt_Lake_City" , "Bleet", null);
-		//cm.addTrack("Las_Vegas", "Los_Angeles" , "Bleet", null);
-		//cm.addTrack("Winnipeg", "Sault_St._Marie" , "Bleet", null);
-		//cm.addTrack("Duluth", "Chicago" , "Yeet", null);
-		//cm.addTrack("Duluth", "Toronto" , "Yeet", null);
-		
-		/*System.out.println(cm.getPlayersLongest());
-		System.out.println(cm.testDP());*/
-		
-		System.out.println(cm.completedContract("Yeet", new ContractCard("Salt Lake City", "Kansas City", 100)));
-		System.out.println(cm.completedContract("Yeet", new ContractCard("Nashville", "New York", 100)));
+		System.out.println("\nblue: "  + cm.completedContract("Bob", new ContractCard("Calgary", "Nashville", 10)));
+		System.out.println("blue: "  + cm.completedContract("Bob", new ContractCard("Salt Lake City", "Kansas City", 10)));
+		System.out.println("red: "  + cm.completedContract("Ron", new ContractCard("Duluth", "El Paso", 10)));
+		System.out.println("green: "  + cm.completedContract("Gary", new ContractCard("Duluth", "El Paso", 10)));
+		System.out.println("yellow: "  + cm.completedContract("Yeet", new ContractCard("Seattle", "Los Angeles", 10)));
 	}
 }
