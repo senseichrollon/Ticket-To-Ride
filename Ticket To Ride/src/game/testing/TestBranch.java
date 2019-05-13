bbe88d60f2f5182dad232ec00565fe0827c91faf
NikunjZamwar <43301538+NikunjZamwar@users.noreply.github.com>
2019-05-12 19:34:50 -0500

    dfs

--- Ticket To Ride/src/game/testing/TestBranch.java
+++ Ticket To Ride/src/game/testing/TestBranch.java
@@ -15,54 +15,52 @@ public class TestBranch
 	public static void main(String[] args) throws IOException
 	{
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("Nikunj");
-		PlayerCardTree testerTree = new PlayerCardTree();
-		testerTree.add("red", 2);
-		testerTree.add("white", 2);
-		testerTree.add("blue", 4);
-		testerTree.add("yellow", 1);
-		testerTree.add("green", 2);
-		testerTree.add("purple", 2);
-		testerTree.add("orange", 2);
-		testerTree.add("wild", 6);
-		testerTree.add("black", 2);
-		
-		
-		System.out.println(testerTree.hasEnough("blue", 10));
-=======
 //		System.out.println("Nikunj");
 //		PlayerCardTree testerTree = new PlayerCardTree();
 //		testerTree.add("red", 2);
 //		testerTree.add("white", 2);
-//		testerTree.add("blue", 2);
-//		testerTree.add("wild", 1);
+//		testerTree.add("blue", 4);
+//		testerTree.add("yellow", 1);
 //		testerTree.add("green", 2);
-//		testerTree.add("pink", 2);
+//		testerTree.add("purple", 2);
 //		testerTree.add("orange", 2);
-//		testerTree.add("wild", 1);
-//		testerTree.add("red", 2);
-//		System.out.println(testerTree);
-//		testerTree.remove("red", 5);
-//		System.out.println(testerTree);
-		
-		Deck test = new Deck();
+//		testerTree.add("wild", 6);
+//		testerTree.add("black", 2);
+//		
+//		
+//		System.out.println(testerTree.hasEnough("blue", 10));
+////		System.out.println("Nikunj");
+////		PlayerCardTree testerTree = new PlayerCardTree();
+////		testerTree.add("red", 2);
+////		testerTree.add("white", 2);
+////		testerTree.add("blue", 2);
+////		testerTree.add("wild", 1);
+////		testerTree.add("green", 2);
+////		testerTree.add("pink", 2);
+////		testerTree.add("orange", 2);
+////		testerTree.add("wild", 1);
+////		testerTree.add("red", 2);
+////		System.out.println(testerTree);
+////		testerTree.remove("red", 5);
+////		System.out.println(testerTree);
+//		
+//		Deck test = new Deck();
+////		System.out.println(Arrays.toString(test.getUpCards()));
+////		test.shuffleIfDeckFinished();
+////		System.out.println(Arrays.toString(test.getUpCards()));
+//		
+////		ArrayList<String> temp = new ArrayList<String>();
+////		temp.add(1);
+////		temp.add(2);
+////		temp.add(3);
+////		System.out.println(temp);
+////		Collections.shuffle(temp);
+////		System.out.println(temp);
 //		System.out.println(Arrays.toString(test.getUpCards()));
-//		test.shuffleIfDeckFinished();
+//		String temp = test.drawTrain(1);
 //		System.out.println(Arrays.toString(test.getUpCards()));
-		
-//		ArrayList<String> temp = new ArrayList<String>();
-//		temp.add(1);
-//		temp.add(2);
-//		temp.add(3);
-//		System.out.println(temp);
-//		Collections.shuffle(temp);
-//		System.out.println(temp);
-		System.out.println(Arrays.toString(test.getUpCards()));
-		String temp = test.drawTrain(1);
-		System.out.println(Arrays.toString(test.getUpCards()));
-		test.shuffle();
-		System.out.println(Arrays.toString(test.getUpCards()));
->>>>>>> branch 'master' of https://github.com/senseichrollon/Ticket-To-Ride.git
+//		test.shuffle();
+//		System.out.println(Arrays.toString(test.getUpCards()));
+
 	}
 }
--- Ticket To Ride/src/game/testing/TestMap.java
+++ Ticket To Ride/src/game/testing/TestMap.java
@@ -15,14 +15,14 @@ public class TestMap
 		
 		//cm.addTrack("Seattle", "Helena" , "Yeet", null);
 		
-		cm.addTrack("Calgary", "Winnipeg" , "Yeet", null);
-		cm.addTrack("Calgary", "Helena" , "Yeet", null);
-		cm.addTrack("Helena", "Winnipeg" , "Yeet", null);
-		cm.addTrack("Winnipeg", "Duluth", "Yeet", null);
-		cm.addTrack("Helena", "Duluth", "Yeet", null);
-		cm.addTrack("Helena", "Omaha" , "Yeet", null);
-		cm.addTrack("Omaha", "Denver" , "Yeet", null);
-		cm.addTrack("Helena", "Denver" , "Yeet", null);
+//		cm.addTrack("Calgary", "Winnipeg" , "Yeet", null);
+//		cm.addTrack("Calgary", "Helena" , "Yeet", null);
+//		cm.addTrack("Helena", "Winnipeg" , "Yeet", null);
+//		cm.addTrack("Winnipeg", "Duluth", "Yeet", null);
+//		cm.addTrack("Helena", "Duluth", "Yeet", null);
+//		cm.addTrack("Helena", "Omaha" , "Yeet", null);
+//		cm.addTrack("Omaha", "Denver" , "Yeet", null);
+//		cm.addTrack("Helena", "Denver" , "Yeet", null);
 		
 		//cm.addTrack("Toronto", "Duluth" , "Yeet", null);
 		//cm.addTrack("Las_Vegas", "Salt_Lake_City" , "Bleet", null);
